c9b3f47699933530bb9b2b3e378dc031bf4fbe23
Killiana <illiana9875@gmail.com>
2021-09-03 12:01:01 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Hello World
+        <footer>
+          {" "}
+          <a
+            href="https://github.com/Killiana/react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open source code{" "}
+          </a>
+          by Iliyana Boneva{" "}
+        </footer>
+      </div>
+    </div>
+  );
 }
